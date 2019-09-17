02c6a4ac0986d8de1d10484206da905eddbffbbc
NicolasJBM <nicolas.jb.mangin@gmail.com>
2019-09-17 15:32:42 +0200

    Correct bug in interaction between text_score_dictionary and bos_dictionary

--- R/text_score_dictionaries.R
+++ R/text_score_dictionaries.R
@@ -1,8 +1,8 @@
 #' Count the occurrences of words associated with a dictionary.
 #' @param x          String or character vector. Conte to be scored.
 #' @param dictionary Tibble. Categorizations of words.
+#' @param category   Character. Name of the grouping variable in the dictionary.
 #' @param type       Character. Whether x is a "string" (in which case it will be split) or a "vector"
-#' @param level      Character. Name of the grouping variable in the dictionary.
 #' @return A tibble with the count of words, for each category and in total, forht the document.
 #' @importFrom dplyr %>%
 #' @importFrom dplyr select
@@ -17,7 +17,7 @@
 #' @export
 
 
-text_score_sentiment <- function(x, dictionary, type = "vector", level = "cluster"){
+text_score_dictionaries <- function(x, dictionary, category = NULL, type = "vector"){
   
   group <- NULL
   words <- NULL
@@ -26,13 +26,13 @@ text_score_sentiment <- function(x, dictionary, type = "vector", level = "cluste
   if (type == "string") x <- unlist(strsplit(tolower(x), split = " ")) else x <- tolower(x)
   
   y <- dictionary %>%
-    dplyr::rename(group = level) %>%
+    dplyr::rename(group = category) %>%
     dplyr::group_by(group) %>%
-    dplyr::summarise(words = paste(words, collapse = "|")) %>%
+    dplyr::summarise(word = paste(word, collapse = "|")) %>%
     dplyr::mutate(
       score = purrr::map_dbl(
-        words,
-        function(words, text) sum(stringr::str_detect(text, words)),
+        word,
+        function(word, text) sum(stringr::str_detect(text, word)),
         text = x
       )
     ) %>%
