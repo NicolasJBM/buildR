ec296a4c87f90fa53ee1d41d5031bc38390687cd
NicolasJBM <nicolas.jb.mangin@gmail.com>
2019-09-20 13:38:59 +0200

    Separate format_ascii for use in writR

--- R/text_clean.R
+++ R/text_clean.R
@@ -16,30 +16,9 @@ text_clean <- function(text){
     length(text) == 1
   )
   
-  format_ascii <- function(text){
-    
-    text <- text %>%
-      stringr::str_replace_all("<.*?>", " ") %>% # remove html tags
-      stringr::str_replace_all("  ", " ") %>% # replace double spaces by single spaces
-      stringr::str_split(" ") %>%
-      unlist()
-    
-    split = strsplit(text, split='')
-    m = lapply(split,match, buildR::bos_toascii$mapL)
-    mapply(function(split,m) paste(ifelse(is.na(m), split, buildR::bos_toascii$mapA[m]), collapse='') , split, m)
-    
-    text <- text %>%
-      stringi::stri_trans_general(id = "ascii") %>%
-      textclean::replace_non_ascii(replacement = "", remove.nonconverted = TRUE)
-    
-    text <- paste(text, collapse = " ")
-    
-    trimws(text)
-  }
-  
   text %>%
     as.character() %>%
-    format_ascii() %>%
+    buildR::bos_format_ascii() %>%
     tm::stripWhitespace() %>%
     trimws()
 }
