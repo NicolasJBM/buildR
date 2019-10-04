3c2ad94850ff1b8094be418e54feb5a89e8fcb3d
NicolasJBM <nicolas.jb.mangin@gmail.com>
2019-10-04 12:09:44 +0200

    rewrite text_apply_dictionaries

--- R/text_apply_dictionaries.R
+++ R/text_apply_dictionaries.R
@@ -5,14 +5,15 @@
 #' @return A tibble with documents as rows, categories as variables, ad the required summary in cells
 #' @importFrom dplyr %>%
 #' @importFrom dplyr select
+#' @importFrom dplyr mutate
 #' @importFrom dplyr ungroup
 #' @importFrom dplyr group_by
 #' @importFrom dplyr summarise
-#' @importFrom dplyr mutate
-#' @importFrom fuzzyjoin regex_inner_join
-#' @importFrom dplyr left_join
+#' @importFrom dplyr filter
 #' @importFrom tidyr spread
-#' @importFrom tibble is_tibble
+#' @importFrom tidyr replace_na
+#' @importFrom stringr str_detect
+#' @importFrom purrr map_dbl
 #' @export
 
 
@@ -30,26 +31,30 @@ text_apply_dictionaries <- function(bow,
   term <- NULL
   category <- NULL
   value <- NULL
-  total <- NULL
-  
-  
+  scores <- NULL
+  counts <- NULL
+
+    
   bow <- dplyr::select(bow, term, count) %>%
-    dplyr::mutate(term = as.character(term))
-  dictionary <- dplyr::select(dictionary, term, category, value) %>%
     dplyr::mutate(term = as.character(term))
   
   nbr_words <- sum(bow$count)
   
-  y <- bow %>%
-    fuzzyjoin::regex_inner_join(dictionary, by = "term", ignore_case = TRUE) %>%
-    dplyr::mutate(total = count * value) %>%
-    dplyr::select(category, total) %>%
+  dictionary <- dplyr::select(dictionary, term, category, value) %>%
+    dplyr::mutate(term = as.character(term)) %>%
+    dplyr::mutate(
+      counts = purrr::map_dbl(term, function(x,y) sum(dplyr::filter(y, stringr::str_detect(term, x))$count), y = bow)
+    ) %>%
+    tidyr::replace_na(list(counts = 0)) %>%
+    dplyr::mutate(scores = counts * value) %>%
     dplyr::group_by(category) %>%
-    dplyr::summarise(total = sum(total, na.rm = TRUE)) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(percentage = total / nbr_words) %>%
-    dplyr::select(category, score = score) %>%
-    tidyr::spread(category, score)
+    dplyr::summarise(scores = sum(scores, na.rm = TRUE)) %>%
+    dplyr::ungroup()
   
-  return(y)
+  if (score == "percentage") dictionary <- dplyr::mutate(dictionary, scores = scores / nbr_words)
+  
+  result <- dictionary %>%
+    tidyr::spread(category, scores, fill = 0)
+  
+  return(result)
 }
