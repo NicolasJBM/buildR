b9c438f9f2ff5abfc00ca1528544e8e38adb025d
NicolasJBM <nicolas.jb.mangin@gmail.com>
2019-10-13 10:34:06 +0200

    Update ascii and add options to text_clean

--- R/text_clean.R
+++ R/text_clean.R
@@ -1,5 +1,9 @@
 #' Remove html tags, remove double spaces, convert to ASCII.
 #' @param text  character. String to be cleaned.
+#' @param clndash logical. Whether spaces should be removed around dashes.
+#' @param clnpunct logical. Whether punctuations should be properly formated (no space before, space after).
+#' @param clnascii logical. Whether non-ascii characters should be replaced by their closest ascii match.
+#' @param clnspc logical. Trim white spaces and prevent double-spaces.
 #' @return A clean string.
 #' @importFrom dplyr %>%
 #' @importFrom stringr str_replace_all
@@ -11,30 +15,55 @@
 #' @importFrom textclean replace_non_ascii
 #' @export
 
-text_clean <- function(text){
+text_clean <- function(text,
+                       clndash = TRUE,
+                       clnpunct = TRUE,
+                       clnascii = TRUE,
+                       clnspc = TRUE){
   
   stopifnot(
     is.character(text),
     length(text) == 1
   )
   
-  text %>%
-    stringr::str_replace_all("-\\s([A-Za-z])", "-\\1") %>%
-    stringr::str_replace_all("\\s-([A-Za-z])", "-\\1") %>%
-    stringr::str_replace_all("\\.([A-Za-z])", ". \\1") %>%
-    stringr::str_replace_all("\\;([A-Za-z])", "; \\1") %>%
-    stringr::str_replace_all("\\,([A-Za-z])", ", \\1") %>%
-    stringr::str_replace_all("\\:([A-Za-z])", ": \\1") %>%
-    stringr::str_replace_all("\\!([A-Za-z])", "! \\1") %>%
-    stringr::str_replace_all("\\?([A-Za-z])", "? \\1") %>%
-    stringr::str_replace_all(stringr::fixed(" ."), stringr::fixed(".")) %>%
-    stringr::str_replace_all(stringr::fixed(" ;"), stringr::fixed(";")) %>%
-    stringr::str_replace_all(stringr::fixed(" ,"), stringr::fixed(",")) %>%
-    stringr::str_replace_all(stringr::fixed(" :"), stringr::fixed(":")) %>%
-    stringr::str_replace_all(stringr::fixed(" !"), stringr::fixed("!")) %>%
-    stringr::str_replace_all(stringr::fixed(" ?"), stringr::fixed("?")) %>%
-    as.character() %>%
-    buildR::bos_format_ascii() %>%
-    tm::stripWhitespace() %>%
-    trimws()
+  clean <- text
+  
+  if (clndash){
+    clean <- clean %>%
+      stringr::str_replace_all("-\\s([A-Za-z])", "-\\1") %>%
+      stringr::str_replace_all("\\s-([A-Za-z])", "-\\1") %>%
+      as.character()
+  }
+  
+  if (clnpunct){
+    clean <- clean %>%
+      stringr::str_replace_all("\\.([A-Za-z])", ". \\1") %>%
+      stringr::str_replace_all("\\;([A-Za-z])", "; \\1") %>%
+      stringr::str_replace_all("\\,([A-Za-z])", ", \\1") %>%
+      stringr::str_replace_all("\\:([A-Za-z])", ": \\1") %>%
+      stringr::str_replace_all("\\!([A-Za-z])", "! \\1") %>%
+      stringr::str_replace_all("\\?([A-Za-z])", "? \\1") %>%
+      stringr::str_replace_all(stringr::fixed(" ."), stringr::fixed(".")) %>%
+      stringr::str_replace_all(stringr::fixed(" ;"), stringr::fixed(";")) %>%
+      stringr::str_replace_all(stringr::fixed(" ,"), stringr::fixed(",")) %>%
+      stringr::str_replace_all(stringr::fixed(" :"), stringr::fixed(":")) %>%
+      stringr::str_replace_all(stringr::fixed(" !"), stringr::fixed("!")) %>%
+      stringr::str_replace_all(stringr::fixed(" ?"), stringr::fixed("?")) %>%
+      as.character()
+  }
+  
+  if (clnascii){
+    clean <- clean %>%
+      buildR::bos_format_ascii() %>%
+      as.character()
+  }
+  
+  if (clnspc){
+    clean <- clean %>%
+      tm::stripWhitespace() %>%
+      trimws() %>%
+      as.character()
+  }
+  
+  return(clean)  
 }
