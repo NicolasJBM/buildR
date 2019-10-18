bab9b5de0a6285537b3bebfea3cb3eaec22aa1d2
NicolasJBM <nicolas.jb.mangin@gmail.com>
2019-10-18 17:56:44 +0200

    Adapt text_apply_stm to the output of text_model_topics.

--- R/text_apply_stm.R
+++ R/text_apply_stm.R
@@ -1,10 +1,9 @@
 #' Apply the topic mode to a new corpus of documents.
-#' @param bow       Tibble. Bag of words as returned by the function text_semrel2bow.
-#' @param stm       stm. Structural topic model as returned by the function stm.
-#' @param labels    Tibble. One variable for "topic", one variable for "label".
-#' @param keywords  Tibble. One variable specifying the "document", one variable containing the "keywords" as a character vector.
-#' @param min_gamma Numeric. Minumum strength of the relationship between topic and socument.
-#' @param min_common Integer. Maximum number of topics allowed per document.
+#' @param bow         Tibble. Bag of words as returned by the function text_semrel2bow.
+#' @param topic_model List. Structural topic model as returned by the function text_model_topics.
+#' @param keywords    Tibble. One variable specifying the "document", one variable containing the "keywords" as a character vector.
+#' @param min_gamma   Numeric. Minumum strength of the relationship between topic and socument.
+#' @param min_common  Integer. Maximum number of topics allowed per document.
 #' @return A tibble linking topics and documents for a new corpus.
 #' @importFrom dplyr mutate
 #' @importFrom dplyr select
@@ -28,8 +27,7 @@
 
 
 text_apply_stm <- function(bow,
-                           stm,
-                           labels = NULL,
+                           topic_model,
                            keywords = NULL,
                            min_gamma = 0.1,
                            min_common = 1){
@@ -51,11 +49,11 @@ text_apply_stm <- function(bow,
   
   newdoc <- stm::textProcessor(doc$term, removestopwords = FALSE, removenumbers = FALSE, removepunctuation = FALSE, stem = FALSE)
   prepdoc <- stm::prepDocuments(newdoc$documents, newdoc$vocab, newdoc$meta)
-  old_vocab <- stm$vocab
+  old_vocab <- topic_model$topic_model$vocab
   
   aligneddoc <- stm::alignCorpus(prepdoc, old.vocab = old_vocab)
   
-  apply_tm <- stm::fitNewDocuments(model = stm, documents = aligneddoc$documents, newData=aligneddoc$meta)
+  apply_tm <- stm::fitNewDocuments(model = topic_model$topic_model, documents = aligneddoc$documents, newData=aligneddoc$meta)
   
   prediction <- as.data.frame(apply_tm$theta)
   
@@ -71,15 +69,14 @@ text_apply_stm <- function(bow,
     dplyr::mutate(topic = as.integer(stringr::str_remove_all(topic, "V"))) %>%
     dplyr::filter(gamma > min_gamma)
   
-  if (!is.null(keywords) & !is.null(labels)){
+  if (!is.null(keywords)){
     
     prediction <- prediction %>%
-      dplyr::left_join(labels, by = "topic") %>%
-      dplyr::mutate(label = purrr::map(label, function(x) unlist(strsplit(x, split = " ")))) %>%
+      dplyr::left_join(dplyr::select(topic_model$topic_term, topic, term), by = "topic") %>%
       dplyr::left_join(keywords, by = "document") %>%
-      dplyr::mutate(common = purrr::map2_dbl(keywords, label, function(x,y) length(intersect(x, y)))) %>%
+      dplyr::mutate(common = purrr::map2_dbl(keywords, term, function(x,y) length(intersect(x, y)))) %>%
       dplyr::mutate(nbkw = purrr::map(keywords, function(x) length(unique(x)))) %>%
-      dplyr::select(-label, -keywords) %>%
+      dplyr::select(-term, -keywords) %>%
       dplyr::filter(nbkw <= 2 | common >= min_common) %>%
       dplyr::select(-nbkw)
     
