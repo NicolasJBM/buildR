d31cf4e8032c5ee32031c2164c8194f309ea0087
NicolasJBM <nicolas.jb.mangin@gmail.com>
2019-10-23 23:50:52 +0200

    Ensure that all words are lower case in bags of words.

--- R/text_semrel2bow.R
+++ R/text_semrel2bow.R
@@ -2,7 +2,6 @@
 #' @param semrel   Tibble. Semantic for one document relationships as returned by text_txt2semrel.
 #' @param basis    Character. Whether a bag of "word", a bag of "lemma", or a bag of "stem" should be returned.
 #' @param keep_pos Character vector. Parts of speech which should be kept to build the bags of words
-#' @param min_count Numeric. Minimum number of occurrences for a term to be kept.
 #' @return A tibble simplified into a bag of words.
 #' @importFrom dplyr %>%
 #' @importFrom dplyr select
@@ -11,19 +10,18 @@
 #' @importFrom dplyr filter
 #' @importFrom dplyr summarise
 #' @importFrom dplyr rename
+#' @importFrom dplyr mutate
 #' @importFrom tibble is_tibble
 #' @export
 
 text_semrel2bow <- function(semrel,
                             basis = "lemma",
-                            keep_pos = c("NOUN", "PROPN", "ADJ", "VERB", "ADV"),
-                            min_count = 1){
+                            keep_pos = c("NOUN", "PROPN", "ADJ", "VERB", "ADV")){
   
   stopifnot(
     tibble::is_tibble(semrel),
     basis %in% c("word","lemma","stem"),
-    is.character(keep_pos),
-    is.numeric(min_count)
+    is.character(keep_pos)
   )
   
   src_word <- NULL
@@ -32,15 +30,22 @@ text_semrel2bow <- function(semrel,
   src_pos <- NULL
   src_token <- NULL
   term <- NULL
+  word <- NULL
+  lemma <- NULL
+  stem <- NULL
   
   semrel %>%
     dplyr::rename(word = src_word, lemma = src_lemma, stem = src_stem) %>%
+    dplyr::mutate(
+      word = tolower(word),
+      lemma = tolower(lemma),
+      stem = tolower(stem)
+    ) %>%
     dplyr::filter(src_pos %in% keep_pos) %>%
     dplyr::select(src_token, term = basis) %>%
     unique() %>%
     dplyr::select(-src_token) %>%
     dplyr::group_by(term) %>%
     dplyr::summarise(count = dplyr::n()) %>%
-    dplyr::filter(count >= min_count) %>%
     dplyr::ungroup()
 }
