3c571191e3d99d7906f09bb26ebc8a8bf2c5dacd
NicolasJBMe <nicolas.jb.mangin@gmail.com>
2020-11-03 11:21:38 +0100

    Extent functions of files_replace_pattern.

--- R/files_replace_pattern.R
+++ R/files_replace_pattern.R
@@ -2,6 +2,8 @@
 #' @param path Character. Absolute path to the files.
 #' @param pattern Character. Pattern to be replaced.
 #' @param replacement Character. Pattern to be put instead.
+#' @param type Character. Extension of the files to be processed
+#' @param folders Logical. Whether subfolders should be included.
 #' @return Replace in the same folder the documents where the string was found by the same documents where the string was replaced.
 #' @importFrom readr read_lines
 #' @importFrom readr write_lines
@@ -11,13 +13,15 @@
 
 files_replace_pattern <- function(path,
                              pattern,
-                             replacement){
+                             replacement,
+                             type = ".Rmd",
+                             folders = FALSE){
   
   stopifnot(
     is.character(path), is.character(pattern), is.character(replacement)
   )
   
-  filenames <- list.files(path = path)
+  filenames <- list.files(path = path, pattern = type, recursive = folders)
   filenames <- paste0(path, filenames)
   
   for(f in filenames){
@@ -26,7 +30,7 @@ files_replace_pattern <- function(path,
     
     if (sum(stringr::str_detect(doc, pattern)) > 0){
       newdoc <- gsub(pattern, replacement, doc)
-      readr::write_lines(newdoc, path=f)
+      readr::write_lines(newdoc, file=f)
     }
     
   }
