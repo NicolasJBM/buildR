4358507ebc8441a99de9d1b566ccf9676627231a
NicolasJBM <31648860+NicolasJBM@users.noreply.github.com>
2021-05-09 14:04:48 +0200

    feat: give control over na.rm in aggreg_cvar

--- R/aggreg_cvar.R
+++ R/aggreg_cvar.R
@@ -3,8 +3,9 @@
 #' @author Nicolas Mangin
 #' @description Computes the coefficient of variation.
 #' @param x numeric vector. Set of values for which the coefficient of variation should be computed.
+#' @param na.rm Logical. Should NAs be omitted.
 #' @return A coefficient of variation for the numeric vector
 #' @importFrom stats sd
 #' @export
 
-aggreg_cvar <- function(x) stats::sd(x, na.rm = TRUE) / mean(x, na.rm = TRUE)
+aggreg_cvar <- function(x, na.rm = TRUE) stats::sd(x, na.rm = na.rm) / mean(x, na.rm = na.rm)
