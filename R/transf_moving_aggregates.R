7b157bef12d753ffe8f6f8a1d35ce44c802193c8
NicolasJBM <31648860+NicolasJBM@users.noreply.github.com>
2021-05-11 14:31:22 +0200

    feat: allow for any moving aggregate.

--- R/transf_moving_aggregates.R
+++ R/transf_moving_aggregates.R
@@ -1,24 +1,25 @@
-#' @name transf_moving_averages
-#' @title Compute moving average values
+#' @name transf_moving_aggregates
+#' @title Compute moving aggregates
 #' @author Nicolas Mangin
 #' @description Compute moving averages or a chronological numeric vector
 #' @param x     Numeric. Vector of chronological values to be transformed into moving averages.
 #' @param range Integer. Number of periods before and after to average with the focal value.
+#' @param fun   Function. Function to apply on the series (e.g. sum, mean, sd or any function taking a numeric vector as input and returns a numeric value)
 #' @return A numeric vector of same size where values are smoothed.
 #' @export
 
-transf_moving_averages <- function(x, range) {
+transf_moving_aggregates <- function(x, range, fun = NULL) {
   stopifnot(
-    length(x >= (2 + range))
+    length(x) >= (2 + range),
+    !is.null(fun)
   )
-
   y <- x
   for (i in seq_len(length(y))) {
     if (i <= range) before <- range - i else before <- range
     if (i >= (length(y) - range)) after <- length(y) - i else after <- range
-    xmin <- i - before
-    xmax <- i + after
-    y[i] <- mean(x[xmin:xmax], na.rm = TRUE)
+    xmin <- max(0,i - before)
+    xmax <- min(length(x), i + after)
+    y[i] <- fun(x[xmin:xmax], na.rm = TRUE)
   }
   return(y)
 }
